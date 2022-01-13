db5ae1531be473ca06130a8112771181f382a49e
nmauersberg <n.mauersberg@gmail.com>
2022-01-13 16:11:56 +0100

    Initialize and configure demo DAO

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { useWeb3 } from '@3rdweb/hooks';
+
 const App = () => {
+  const { connectWallet, address } = useWeb3();
+  if (!address) {
+    return (
+      <div className='landing'>
+        <h1>The HealthCareDAO</h1>
+        <button onClick={() => connectWallet('injected')} className='btn-hero'>
+          Connect with MetaMask
+        </button>
+      </div>
+    );
+  }
+
+  console.log('Connected to Address:', address);
   return (
-    <div className="landing">
-      <h1>Welcome to My DAO</h1>
+    <div className='landing'>
+      <h1>MetaMask Wallet connected!</h1>
     </div>
   );
 };
